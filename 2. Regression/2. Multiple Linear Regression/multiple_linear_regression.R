18a125ac3006edec00cb31f61ac7f9888e7a8035
zhangshiqiu <zhangshiqiu1993@126.com>
2017-05-23 19:54:54 -0700

    multiple linear regression (r version)

---
+++ 2. Regression/2. Multiple Linear Regression/multiple_linear_regression.R
@@ -0,0 +1,20 @@
+dataset = read.csv('50_Startups.csv')
+
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+library(caTools)
+
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+regressor = lm(formula = Profit ~ .,data = training_set)
+summary(regressor)
+
+y_pred = predict(regressor, newdata = test_set)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+summary(regressor)
